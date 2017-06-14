98106737031c24bf21f461181c416449d200b672
Artyom Androsov <aan@virtoway.com>
2017-06-14 12:40:26 +0200

    Initial module structure

---
+++ VirtoCommerce.B2BExtentionsModule.Web/Module.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Practices.Unity;
+using VirtoCommerce.Platform.Core.Modularity;
+
+namespace VirtoCommerce.B2BExtentionsModule.Web
+{
+    public class Module : ModuleBase
+    {
+        private const string _connectionStringName = "VirtoCommerce";
+        private readonly IUnityContainer _container;
+
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        #region IModule Members
+
+        public override void SetupDatabase()
+        {
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void PostInitialize()
+        {
+        }
+
+        #endregion
+    }
+}
